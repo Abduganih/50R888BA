50834351fd4388a64ca0d485e8e6a0132b3ed119
50r888ba-cyber <sherhon72@gmail.com>
2026-07-20 19:15:10 +0500

    Connect MainActivity with AIService

--- app/src/main/java/com/fiftyr888ba/app/MainActivity.java
+++ app/src/main/java/com/fiftyr888ba/app/MainActivity.java
@@ -2,12 +2,20 @@ package com.fiftyr888ba.app;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.fiftyr888ba.app.service.AIService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AIService aiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aiService = new AIService();
+
+        String javob = aiService.ask("Assalomu alaykum");
+        System.out.println(javob);
     }
 }
